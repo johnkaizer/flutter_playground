15b6bba4c3f8c79bce2384793d009bae8c437a73
john kaizer <johnkaiser964@gmail.com>
2021-09-14 16:44:54 +0300

    word generator

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,47 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
-class Counter extends StatefulWidget {
-  const Counter({Key? key}) : super(key: key);
+void main() => runApp(MyApp());
 
+class MyApp extends StatelessWidget {
   @override
-  _CounterState createState() => _CounterState();
-}
-
-class _CounterState extends State<Counter> {
-  int _counter = 0;
-
-  void _increment() {
-    setState(() {
-      _counter++;
-    });
+  Widget build(BuildContext context){
+    final wordPair = WordPair.random();
+    return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.green[900]),
+      home: RandomWords());
   }
-
+}
+class  RandomWords extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        ElevatedButton(
-          onPressed: _increment,
-          child: const Text('Increment'),
-        ),
-        const SizedBox(width: 16),
-        Text('Count: $_counter'),
-      ],
+  RandomWordsState createState() => RandomWordsState();
+}
+class RandomWordsState extends State<RandomWords>{
+  final _randomWordPairs = <WordPair>[];
+  Widget _buildList(){
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder:(context, item){
+        if(item.isOdd) return Divider();
+        final index = item ~/2;
+        if(index >= _randomWordPairs.length){
+          _randomWordPairs.addAll(generateWordPairs().take(10));
+
+        }
+        return _buildRow(_randomWordPairs[index]);
+    },
     );
   }
-}
+  Widget _buildRow(WordPair pair){
+    return ListTile(title: Text(pair.asPascalCase,
+    style: TextStyle(fontSize: 18.0),));
+  }
 
-void main() {
-  runApp(
-    const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Counter(),
-        ),
-      ),
-    ),
-  );
+  Widget build(BuildContext context){
+    return Scaffold (
+      appBar: AppBar(title: Text('WordPair Generator')),
+      body:_buildList());
+  }
 }
